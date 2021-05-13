6798e69a5e1dcf0b34f03a99eefd27cd869f5a1b
Harshil <harshildarji2001@gmail.com>
2021-05-12 19:37:03 -0700

    :sleepy: fixes to landing page, yet to fix sign in page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,55 @@
-import React, { useState } from 'react';
-import './Login.css';
+import React, { useState } from "react";
+import "./Login.css";
 import flixnetLogo from "../assets/images/flixnet.png";
-import SignUp from './SignUp';
+import SignUp from "./SignUp";
 
 function Login() {
+  const [signIn, setSignIn] = useState(false);
 
-    const [signIn, setSignIn] = useState(false);
-
-    return (
-        <div className="login">
+  return (
+    <React.Fragment>
+      {signIn ? (
+        <SignUp />
+      ) : (
+        <>
+          <div className="login">
             <div className="login__background">
-                <img className="login__logo" src={flixnetLogo} alt="Flixnet" />
-                <button className="signin__button" onClick={() => setSignIn(true)}>Sign in</button>
-                <div className="login__gradient"></div>
-            </div>
-            <div className="login__body">
-                {signIn ? (<SignUp />) : (<>
+              <img className="login__logo" src={flixnetLogo} alt="Flixnet" />
+              <button
+                className="signin__button"
+                onClick={() => setSignIn(true)}
+              >
+                Sign in
+              </button>
+              <div className="login__card">
+                <div className="login__gradient">
+                  <div className="login__content">
                     <h1>Unlimited movies, TV shows and more.</h1>
                     <h2>Watch anywhere. Cancel anytime.</h2>
-                    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                    <h3>
+                      Ready to watch? Enter your email to create or restart your
+                      membership.
+                    </h3>
                     <div className="login__input">
-                        <form>
-                            <input type="email" placeholder="Email Address" />
-                            <button className="login__getStarted" onClick={() => setSignIn(true)}>GET STARTED</button>
-                        </form>
+                      <form>
+                        <input type="email" placeholder="Email Address" />
+                        <button
+                          className="login__getStarted"
+                          onClick={() => setSignIn(true)}
+                        >
+                          GET STARTED
+                        </button>
+                      </form>
                     </div>
-                </>)
-                }
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        </>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default Login;
